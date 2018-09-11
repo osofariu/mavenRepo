e5d6fe60787bd2d3dfa0356613bf480c01eed51e
Ovi Sofariu <osofariu@pillartechnology.com>
2018-09-11 12:31:07 -0400

    changes made

---
+++ src/main/java/play/aep/App.java
@@ -0,0 +1,18 @@
+package play.aep;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String s = sayHello();
+        System.out.println(s);
+    }
+
+    public static String sayHello() {
+        return "Hello World!";
+    }
+}
