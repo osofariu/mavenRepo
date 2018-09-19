34e354de659deeab665c993eabb493b86617c2dc
Ovi Sofariu <osofariu@pillartechnology.com>
2018-09-19 09:17:56 -0400

    mvn verify -Denv=qa runs verifier only for this env

--- src/main/java/play/aep/App.java
+++ src/main/java/play/aep/App.java
@@ -1,18 +1,36 @@
 package play.aep;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+import org.apache.commons.io.IOUtils;
+import java.nio.charset.StandardCharsets;
+
+public class App {
+    public static void main(String[] args) {
         String s = sayHello();
-        System.out.println(s);
+        App app = new App();
+        String rs = app.getMyResource();
+
+        System.out.printf("%s from %s\n", s, rs);
     }
 
     public static String sayHello() {
         return "Hello World!";
+    }
+
+    public String getMyResource() {
+        InputStream is = getClass().getResourceAsStream("prop1.main");
+        StringWriter writer = new StringWriter();
+        if (is == null) {
+            return "NULL";
+        } else {
+            try {
+                IOUtils.copy(is, writer, StandardCharsets.UTF_8);
+            } catch (IOException ioe) {
+                System.err.println("ERROR " + ioe.getMessage());
+            }
+            return writer.toString();
+        }
     }
 }
